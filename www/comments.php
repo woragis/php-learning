78563a7cb4245bcdba2caacc4c8fd78dce6ce966
woragis <masteringthecode.woragis+github@gmail.com>
2024-05-25 00:32:58 -0300

    Comments in php

---
+++ www/comments.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Comments page</title>
+</head>
+<body>
+  <?php
+    // Comments
+    echo "Comments are fun"; // comments are indeed fun
+    echo "<br>";
+    echo "Comments are useful for <strong>describing</strong> whats being done in the code <br>";
+    echo "and for adding <strong>documentation</strong> <br>";
+    echo "and it is useful because we can forget why we were writing that code in the first place";
+    echo "<br>";
+    echo "It is useful for when we dont want to execute some command too";
+    // like this
+    // echo "hr";
+  ?>
+</body>
+</html>
