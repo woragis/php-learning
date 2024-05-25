a014c7ee5f35d7e4cc786ea9c31da83cfcacc1b5
woragis <masteringthecode.woragis+github@gmail.com>
2024-05-24 23:50:11 -0300

    Create simple calculator

---
+++ www/complex_calculator.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <title>Conditions Page</title>
+  </head>
+	<body>
+    <h1>Conditions</h1>
+    <h3>Calculator</h3>
+    <form action="calculator.php" method="post" >
+      <label for="number1">Number 1: </label> <input type="text" name="number1" id="number1"> <br>
+      <label for="number1">Number 2: </label> <input type="text" name="number2" id="number2"> <br>
+      <label for="operator">Operator: </label> <input type="text" name="operator" id="operator"> <br>
+      <input type="submit" value="Calculate">
+    </form>
+    <?php
+      $number1 = $_POST["number1"];
+      $number2 = $_POST["number2"];
+      $operator = $_POST["operator"];
+      if ($operator == "+") {
+        echo $number1 + $number2;
+      }
+      elseif ($operator == "-") {
+        echo $number1 - $number2;
+      }
+      elseif ($operator == "*") {
+        echo $number1 * $number2;
+      }
+      elseif ($operator == "/") {
+        echo $number1 / $number2;
+      } else {
+        echo "Invalid Operator";
+      }
+    ?>
+	</body>
+</html>
