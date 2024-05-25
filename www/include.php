394e42ea8056afc575857bbc1b159d475bec48ae
woragis <masteringthecode.woragis+github@gmail.com>
2024-05-25 02:01:22 -0300

    Learn include on php

---
+++ www/include.php
@@ -0,0 +1,9 @@
+<?php include "include_nav.php" ?>
+<?php include "include_footer.php" ?>
+<?php 
+  // nav variables
+$pageLanguage = "en";
+$pageTitle = "Include Page";
+$description = "Page about discovering new php possibilities and use for templates and DRY code writing";
+$tags = array("php", "include", "template", "functionalities");
+?>
