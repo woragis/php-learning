aada9bb4957492a2e66db4290b402a306203b0f4
woragis <masteringthecode.woragis+github@gmail.com>
2024-05-25 00:29:27 -0300

    mend

--- www/loops_for.php
+++ www/loops_for.php
@@ -9,6 +9,11 @@
     for ($i = 0; $i < 10; $i++) {
       echo "Position: $i <br>";
     }
+    $friends = array("Israel", "Jefferson", "Nicolas", "Rafael");
+    for ($i = 0; $i < count($friends); $i++) {
+      echo $friends[$i];
+      echo "<br>";
+    }
   ?>
   
 </body>
